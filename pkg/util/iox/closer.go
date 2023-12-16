fab4c1e73ebeff61bbe7028628d3479f198db6e4
Henning Korsholm Rohde <herohde@seekerror.org>
2023-12-16 14:41:28 -0800

    Add async closer helpers

--- pkg/util/iox/closer.go
+++ pkg/util/iox/closer.go
@@ -1,6 +1,9 @@
 package iox
 
-import "sync/atomic"
+import (
+	"context"
+	"sync/atomic"
+)
 
 // AsyncCloser is a guarded quit chan. It is safer and more convenient by allowing
 // multiple calls to Close.
@@ -22,6 +25,10 @@ func NewAsyncCloser() AsyncCloser {
 	return &closer{quit: make(chan struct{})}
 }
 
+func NewAsyncCloserWithCancel(ctx context.Context) AsyncCloser {
+	return WithCancel(ctx, NewAsyncCloser())
+}
+
 func (c *closer) IsClosed() bool {
 	return c.closed.Load()
 }
@@ -34,4 +41,22 @@ func (c *closer) Close() {
 	if c.closed.CompareAndSwap(false, true) {
 		close(c.quit)
 	}
+}
+
+// WithCancel closes the closer if the context is cancelled. Returns the closer for convenience.
+func WithCancel(ctx context.Context, closer AsyncCloser) AsyncCloser {
+	return WithQuit(ctx.Done(), closer)
+}
+
+// WithQuit closes the closer if the quit chan closes. Returns the closer for convenience.
+func WithQuit(quit <-chan struct{}, closer AsyncCloser) AsyncCloser {
+	go func() {
+		select {
+		case <-quit:
+			closer.Close()
+		case <-closer.Closed():
+		}
+	}()
+
+	return closer
 }
